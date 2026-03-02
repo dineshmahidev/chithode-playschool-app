7d286e2515f911fd4ca636ced5c55e8b5832fb1c
dineshmahidev <dinesh.mahi.dev@gmail.com>
2026-03-02 19:41:19 +0530

    fix: adjust babel preset order for production bundling

--- my-expo-app/babel.config.js
+++ my-expo-app/babel.config.js
@@ -2,11 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
     plugins: [
-      "nativewind/babel", // 🔥 MUST BE HERE
-      "react-native-reanimated/plugin"
+      "react-native-reanimated/plugin",
     ],
   };
 };

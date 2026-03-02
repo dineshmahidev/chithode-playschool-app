9f38a59f2e71b3cee4290368590f0d5362df79eb
dineshmahidev <dinesh.mahi.dev@gmail.com>
2026-03-02 19:28:27 +0530

    debug: add verbose logging and simplify metro config

--- my-expo-app/metro.config.js
+++ my-expo-app/metro.config.js
@@ -3,8 +3,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-// Force Metro to prioritize react-native and browser fields
-// This prevents the "crypto" error in Axios 1.7+ by avoiding the node-specific distribution
-config.resolver.resolverMainFields = ['react-native', 'browser', 'main'];
-
 module.exports = withNativeWind(config, { input: './global.css' });
